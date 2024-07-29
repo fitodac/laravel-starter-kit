f909316d1576612e3b7c2dab820e90a8626cec87
fitodac <fitodac@gmail.com>
2024-07-29 17:58:17 -0300

    Database demo

---
+++ database/seeders/demo/UsersSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders\demo;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		User::factory()->create([
+			'name' => 'John Doe',
+			'email' => 'admin@local.com'
+		]);
+	}
+}
